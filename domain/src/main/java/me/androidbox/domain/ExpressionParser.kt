525008284ee26f42c08f0fff8120deef96376969
Steve <steve@kitkat@gmail.com>
2022-08-30 09:53:13 +0700

    Adds logic for doing parsing and calculations

---
+++ domain/src/main/java/me/androidbox/domain/ExpressionParser.kt
@@ -0,0 +1,8 @@
+package me.androidbox.domain
+
+class ExpressionParser(private val calculation: String) {
+
+    fun parse(): List<ExpressionPart> {
+        return emptyList()
+    }
+}
---
+++ domain/src/main/java/me/androidbox/domain/Operation.kt
@@ -0,0 +1,21 @@
+package me.androidbox.domain
+
+import java.lang.IllegalArgumentException
+
+enum class Operation(val symbol: Char) {
+    ADD('+'),
+    SUBTRACT('-'),
+    MULTIPLY('X'),
+    DIVIDE('/'),
+    PERCENT('%')
+}
+
+val operationSymbols = Operation.values().map { operation ->
+    operation.symbol
+}.joinToString("")
+
+fun operationFromSymbol(symbol: Char): Operation {
+    return Operation.values().firstOrNull { operation ->
+        operation.symbol == symbol
+    } ?: throw IllegalArgumentException("This symbol [ $symbol ] does not exist")
+}
